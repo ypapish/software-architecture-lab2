ce88f2f699460f1512f4ad0bf7b93642ed809238
Myroslava Saveniuk <143611799+saveniukk@users.noreply.github.com>
2025-03-20 17:51:00 +0200

    added postfix to lisp convertor

--- implementation.go
+++ implementation.go
@@ -1,9 +1,52 @@
 package lab2
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
 
-// TODO: document this function.
-// PrefixToPostfix converts
-func PrefixToPostfix(input string) (string, error) {
-	return "TODO", fmt.Errorf("TODO")
+func isOperator(element string) bool {
+	return element == "+" || element == "-" || element == "*" || element == "/" || element == "^"
+}
+
+func isNumber(s string) bool {
+	_, err := strconv.Atoi(s) 
+	return err == nil        
+}
+
+func postfixToLisp(expression string) (string, error) {
+	var stack []string
+	elements := strings.Fields(expression)
+
+	for _, element := range elements {
+		if !isNumber(element) && !isOperator(element) {
+			return "", fmt.Errorf("invalid expression: invalid element '%s'", element)
+		}
+
+		if isOperator(element) {
+			if len(stack) < 2 {
+				return "", fmt.Errorf("invalid expression: incorrect number of operands for '%s'", element)
+			}
+
+			operand1 := stack[len(stack)-2]
+			operand2 := stack[len(stack)-1]
+			stack = stack[:len(stack)-2]
+
+			operator := element
+			if element == "^" {
+				operator = "pow"
+			}
+
+			lispExpr := fmt.Sprintf("(%s %s %s)", operator, operand1, operand2)
+			stack = append(stack, lispExpr)
+		} else {
+			stack = append(stack, element)
+		}
+	}
+
+	if len(stack) != 1 {
+		return "", fmt.Errorf("invalid expression: incorrect number of elements")
+	}
+	return stack[0], nil
 }

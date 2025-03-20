ed86c1697aeedc3a7f2e25678b8a5225f6ebfff1
Yana <y.papish14@gmail.com>
2025-03-20 20:36:38 +0200

    add ComputeHandler struct

--- handler.go
+++ handler.go
@@ -1,9 +1,12 @@
 package lab2
 
-// ComputeHandler should be constructed with input io.Reader and output io.Writer.
-// Its Compute() method should read the expression from input and write the computed result to the output.
+import (
+	"io"
+)
+
 type ComputeHandler struct {
-	// TODO: Add necessary fields.
+	Input  io.Reader
+	Output io.Writer
 }
 
 func (ch *ComputeHandler) Compute() error {
